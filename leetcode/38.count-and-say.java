fc0d50dffad5f472dda5ff14bdb94063c3318fa8
duwen <1369822573@qq.com>
2019-09-30 17:38:18 +0800

    leetcode:38

---
+++ leetcode/38.count-and-say.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=38 lang=java
+ *
+ * [38] Count and Say
+ */
+
+// @lc code=start
+class Solution {
+    public String countAndSay(int n) {
+        
+    }
+}
+// @lc code=end
+
